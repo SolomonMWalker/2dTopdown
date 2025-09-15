e05e6fbe35e307125697fa47f0a3766d787f3b33
Solomon Walker <solomonmwalker@gmail.com>
2025-09-15 16:10:12 -0400

    first

---
+++ Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+    [Export] public int speed = 300;
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        var movement = Vector2.Zero;
+        
+        if (Input.IsKeyPressed(Key.W))
+        {
+            movement.Y -= 1;
+        }
+        if (Input.IsKeyPressed(Key.S))
+        {
+            movement.Y += 1;
+        }
+        if (Input.IsKeyPressed(Key.D))
+        {
+            movement.X += 1;
+        }
+        if (Input.IsKeyPressed(Key.A))
+        {
+            movement.X -= 1;
+        }
+
+        Velocity = movement * speed;
+
+        MoveAndSlide();
+        
+        LookAt(GetGlobalMousePosition());
+
+        if (Input.IsMouseButtonPressed(MouseButton.Left))
+        {
+            
+        }
+    }
+}
